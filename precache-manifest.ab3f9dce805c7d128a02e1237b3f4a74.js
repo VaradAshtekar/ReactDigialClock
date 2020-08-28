ec2d186fcdd40aea84eb2c18e87925c457d7e95f
VaradAshtekar <varadashtekar96@gmail.com>
2020-08-28 15:49:33 +0530

    Updates

---
+++ precache-manifest.ab3f9dce805c7d128a02e1237b3f4a74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9a3c56590bf5abf7d19f791f98eb7f9",
+    "url": "/ReactDigialClock/index.html"
+  },
+  {
+    "revision": "d92f0e1e2cb3ea6f98d9",
+    "url": "/ReactDigialClock/static/css/main.9bb7c9e1.chunk.css"
+  },
+  {
+    "revision": "800e77403934d29382ce",
+    "url": "/ReactDigialClock/static/js/2.5049f2c4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactDigialClock/static/js/2.5049f2c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d92f0e1e2cb3ea6f98d9",
+    "url": "/ReactDigialClock/static/js/main.c0ec283f.chunk.js"
+  },
+  {
+    "revision": "2610a0a8cbec788c9db1",
+    "url": "/ReactDigialClock/static/js/runtime-main.4d531507.js"
+  }
+]);
